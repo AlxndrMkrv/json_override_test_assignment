de54874c48b0d1ca1b734acce2b26460ea85e1f8
Alexander Makarov <alexander.makarov@live.com>
2023-09-24 22:49:53 +0300

    small fixes

--- json_override_test_assignment/_paths.py
+++ json_override_test_assignment/_paths.py
@@ -1,5 +1,5 @@
 """Module contains paths to "overrides.json" file and directory with
 "config*.json" files"""
 
-OVERRIDES = "/tmp/overrides.json"  # "/etc/overrides.json"
-CONFIGS = "/tmp/configs"  # "/usr/share/json_override_test_assignment/"
+OVERRIDES = "/etc/overrides.json"
+CONFIGS = "/usr/share/json_override_test_assignment/"
